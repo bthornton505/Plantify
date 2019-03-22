5ab67432a7e8287a0da150bb7b791453c24574d2
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-22 15:29:31 -0600

    created reducers directory with index file to create combinereducer which created rootReducer

---
+++ pbc-client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import authReducers from './authReducers';
+
+const rootReducer = combineReducers({
+  auth: authReducers
+})
+
+export default rootReducer;
