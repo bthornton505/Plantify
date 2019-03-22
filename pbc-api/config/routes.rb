98230af9d195f6c5ee6afcc85cd162e148a1aaad
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-22 12:31:13 -0600

    fixed spelling mistake

--- pbc-api/config/routes.rb
+++ pbc-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    recources :users
+    resource :users
     post 'user_token' => 'user_token#create'
     post 'find_user' => 'users#find'
   end
