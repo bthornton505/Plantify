7f98fde80bf3c942f39f682294271fd4d2167e6f
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-22 15:30:43 -0600

    wrapped app in redux provider and created inital store

--- pbc-client/src/index.js
+++ pbc-client/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index.js'
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
