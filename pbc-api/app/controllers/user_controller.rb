dcaf8744f0211b7e6397ce7b69658fe125d169a3
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-22 12:16:35 -0600

    created find action to find a user based on email params and render json user object on success or error on failure

--- pbc-api/app/controllers/user_controller.rb
+++ pbc-api/app/controllers/user_controller.rb
@@ -1,3 +1,13 @@
 class UserController < ApplicationController
 
-end 
+  def find
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      render json: @user
+    else
+      @errors = @user.errors.full_messages
+      render json: @errors
+    end
+  end
+
+end
