5c410a0aeea4f267cfed6cca4f21f92246747ac4
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-22 12:17:15 -0600

    created api namespace for routes and created user resources and route for finding user action

--- pbc-api/config/routes.rb
+++ pbc-api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    recources :users
+    post 'user_token' => 'user_token#create'
+    post 'find_user' => 'users#find'
+  end
+
 end
