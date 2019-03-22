f6d9269e219128408f21180e4bda4981a5e2c2de
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-22 17:50:57 -0600

    added const and logic to determine what routes to show user

--- pbc-client/src/App.js
+++ pbc-client/src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import SignupForm from './components/SignupForm';
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Signup from './components/Signup';
+import UserProfile from './containers/users/UserProfile'
 import './App.css';
 
 class App extends Component {
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Plant Based Chef</h1>
-          <SignupForm />
-        </header>
+
+    const {isAuthenticated, user} = this.props
+
+    const guestViews = (
+      <div>
+        <h1>Plant Based Chef</h1>
+        <Route exact path="/signup" component={Signup} />
       </div>
+    )
+
+    const userViews = (
+      <div>
+        <h1>Plant Based Chef</h1>
+        <Route exact path="/user_profile" render={() => <UserProfile user={user}/>} />
+      </div>
+    )
+
+    return (
+      <Router>
+        {isAuthenticated ? userViews : guestViews}
+      </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    user: state.auth.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(App);
