902522cbe58474e3d7149f6b1a0a43b3ddb354c1
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-23 11:14:54 -0600

    added validations for username and email

--- pbc-api/app/models/user.rb
+++ pbc-api/app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :comments
 
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true 
+
   has_secure_password
 end
