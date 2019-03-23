b3274e20e769fc8ff0a33a09b4a8d84eec5cb29d
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-23 10:34:52 -0600

    updated user resources to proper spelling

--- pbc-api/config/routes.rb
+++ pbc-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resource :users
+    resources :users
     post 'user_token' => 'user_token#create'
     post 'find_user' => 'users#find'
   end
