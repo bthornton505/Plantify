ae76b5cb6b9ebf446901ef79d54db1ca0f67f8a5
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-23 11:46:10 -0600

    updated to render user props

--- pbc-client/src/containers/users/UserProfile.js
+++ pbc-client/src/containers/users/UserProfile.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 
 class UserProfile extends Component {
-  // constructor(props) {
-  //   super(props)
-  //
-  //   this.state = {
-  //     user: this.state.user
-  //   }
-  // }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: this.props.user
+    }
+  }
 
   render() {
     return(
       <div>
         <h1>User Profile</h1>
+        <div>
+          <p>
+            <label>Username: </label>
+            {this.props.user.username}
+          </p>
+          <p>
+            <label>Email: </label>
+            {this.props.user.email}
+          </p>
+        </div>
       </div>
     );
   }
