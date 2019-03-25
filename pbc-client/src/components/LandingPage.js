bd12c3e2cae150090780a9636486b92a7f4d4654
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-25 10:09:18 -0600

    created a basic landing page with a link to the signup page

---
+++ pbc-client/src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage= () => {
+
+  return(
+    <div>
+      <h1>Welcome to the Plant Based Chef!</h1>
+      <p>We provide the easiest and way to track your favorite plant-based recipes and share them with friends and family.</p>
+      <p>Not only can you create and save your fav recipes, but you can also explore our vast database of vegan/vegetarian recipes!</p>
+      <p>If this sounds like something for you, sign up!</p>
+      <Link to="/signup">Sign Up</Link>
+    </div>
+  )
+}
+
+export default LandingPage;
