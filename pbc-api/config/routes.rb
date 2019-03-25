a9d7929dedee48e885d1bec3e18f8af5988a8494
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-25 13:10:36 -0600

    created resources for recipes

--- pbc-api/config/routes.rb
+++ pbc-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :users
+    resources :users, :recipes
     post 'user_token' => 'user_token#create'
     post 'find_user' => 'users#find'
   end
