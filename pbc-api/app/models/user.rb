0bbc605a0dfe877a17f5eb78a3b3a1ef60cbc5df
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-25 12:04:05 -0600

    added method to define token

--- pbc-api/app/models/user.rb
+++ pbc-api/app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :recipes
   has_many :comments
 
   validates :username, uniqueness: true
-  validates :email, uniqueness: true 
+  validates :email, uniqueness: true
 
-  has_secure_password
+  def to_token_payload
+    {
+      user: id,
+      email: email
+    }
+  end
+  
 end
