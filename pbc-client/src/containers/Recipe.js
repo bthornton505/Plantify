9856b45814741d84c595f8f7ba948d2d81456eb1
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-26 16:06:02 -0600

    popped in allRecipes componenet to see if it works

--- pbc-client/src/containers/Recipe.js
+++ pbc-client/src/containers/Recipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AllRecipes from '../components/recipes/AllRecipes';
 import RecipeForm from '../components/recipes/RecipeForm';
 
 class Recipe extends Component {
@@ -12,6 +13,8 @@ class Recipe extends Component {
   render() {
     return(
       <div>
+        <AllRecipes />
+        <br></br>
         <h1>New Recipe</h1>
         <p>Wanna make a new recipe? Go for it!</p>
         This will be a recipe form... maybe?
