bb9b99c67df9d5cff6a5454d1d441370e8e6d579
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-26 15:41:36 -0600

    fixed some spelling

--- pbc-client/src/containers/users/UserProfile.js
+++ pbc-client/src/containers/users/UserProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import AllRecipes from '.../components/recipes/AllRecipes';
+import AllRecipes from '../components/recipes/AllRecipes';
 
 class UserProfile extends Component {
   constructor(props) {
@@ -35,7 +35,7 @@ class UserProfile extends Component {
             <label>Email: </label>
             {this.props.user.email}
           </p>
-          <AllRecipes recipes={this.prop.recipe}/>
+          <AllRecipes recipes={this.prop.recipes}/>
           <Link to="/new_recipe">Create Recipe</Link>
         </div>
       </div>
