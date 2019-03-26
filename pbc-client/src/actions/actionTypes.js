ff8e223cdee96414a0a4fb46b002f0f717cbf22a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-26 15:40:41 -0600

    deleted edit recipe action and made request recipes

--- pbc-client/src/actions/actionTypes.js
+++ pbc-client/src/actions/actionTypes.js
@@ -4,7 +4,7 @@ export const AUTHENTICATION_FAILURE = "AUTHENTICATION_FAILURE"
 export const LOGOUT = "LOGOUT"
 
 // Recipe Action Types
+export const REQUEST_RECIPES = "REQUEST_RECIPES"
 export const CREATE_RECIPE = "CREATE_RECIPE"
-export const EDIT_RECIPE = "EDIT_RECIPE"
 export const UPDATE_RECIPE = "UPDATE_RECIPE"
 export const DELETE_RECIPE = "DELETE_RECIPE"
