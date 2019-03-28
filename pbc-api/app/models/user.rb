82c57b3f79b02c95960881920e053ae5e5459589
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-28 11:41:44 -0600

    added validation for presence of password digest

--- pbc-api/app/models/user.rb
+++ pbc-api/app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates_presence_of :password_digest
 
 end
