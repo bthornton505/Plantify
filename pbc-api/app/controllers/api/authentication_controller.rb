dabcb05a1588c9520b355411aa881b6064edcb2b
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-28 11:40:32 -0600

    updated authenticate action and added auth_params

--- pbc-api/app/controllers/api/authentication_controller.rb
+++ pbc-api/app/controllers/api/authentication_controller.rb
@@ -1,14 +1,16 @@
 class Api::AuthenticationController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :authorize_request, only: :authenticate
 
   def authenticate
-    command = AuthenticateUser.call(params[:username], params[:email], params[:password])
+    auth_token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
 
-    if command.success?
-      render json: { auth_token: command.result }
-    else
-      render json: { error: command.errors }, status: :unauthorized
-    end
+  private
+
+  def auth_params
+    params.permit(:email, :password)
   end
 
 end
