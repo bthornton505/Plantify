98937001b6e0dac809f1b23a20bf9eaf54e9ea18
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-28 11:41:16 -0600

    updated private method to use new jwt classes

--- pbc-api/app/controllers/application_controller.rb
+++ pbc-api/app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_request
+  include Response
+  include ExceptionHandler
+
+  # called before every action on controllers
+  before_action :authorize_request
   attr_reader :current_user
 
   private
 
-  def authenticate_request
-    @current_user = AuthorizeApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  # Check for valid request token and return user
+  def authorize_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+    # render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 
 end
