6db089e0c5c14d84f38327eb491f26bb28477082
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-28 11:42:28 -0600

    added signup and auth/login routes

--- pbc-api/config/routes.rb
+++ pbc-api/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, :recipes
     # get 'auth' => 'auth#auth'
-    post 'authenticate', to: 'authentication#authenticate'
+    post 'signup', to: 'users#create'
+    post 'auth/login', to: 'authentication#authenticate'
     post 'find_user' => 'users#find'
   end
 
