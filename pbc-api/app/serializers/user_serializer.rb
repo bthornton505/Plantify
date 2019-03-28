3eb84a7f64f61b9927195c7a9ce7c28ba4403559
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-28 11:42:02 -0600

    removed unused method and added attribute of id

--- pbc-api/app/serializers/user_serializer.rb
+++ pbc-api/app/serializers/user_serializer.rb
@@ -1,11 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :email
+  attributes :id, :username, :email
   has_many :recipes
-
-  def serializable_hash(adapter_options = nil, options = {}, adapter_instance = self.class.serialization_adapter_instance)
-    hash = super
-    hash.each { |key, value| hash.delete(key) if value.nil? }
-    hash
-  end
 
 end
