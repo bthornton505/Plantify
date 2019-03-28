cd6e7549952aaa9159f07460fb8c806aca708bec
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-27 21:18:20 -0600

    removed autoload of root lib directory

--- pbc-api/config/application.rb
+++ pbc-api/config/application.rb
@@ -22,7 +22,7 @@ module PbcApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.autoload_paths << Rails.root.join('lib')
+    # config.autoload_paths << Rails.root.join('lib')
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
