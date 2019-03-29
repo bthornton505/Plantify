5455ecd1a8b5dfebf467b950dc1f4947430ea9f0
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-29 10:46:59 -0600

    connected recipe.js to redux and passed recipes prop to allrecipes component

--- pbc-client/src/containers/Recipe.js
+++ pbc-client/src/containers/Recipe.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { fetchRecipes } from '../actions/recipeActions'
 import AllRecipes from '../components/recipes/AllRecipes';
 import RecipeForm from '../components/recipes/RecipeForm';
 
@@ -6,14 +9,19 @@ class Recipe extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      user_id: this.props.user
+      user_id: this.props.user,
+      recipes: this.props.user.recipes
     }
+  }
+
+  componentDidMount = () => {
+    this.props.fetchRecipes()
   }
 
   render() {
     return(
       <div>
-        <AllRecipes />
+        <AllRecipes recipes={ this.state.recipes }/>
         <br></br>
         <p>Wanna make a new recipe? Go for it!</p>
         <h1>New Recipe</h1>
@@ -23,4 +31,4 @@ class Recipe extends Component {
   }
 }
 
-export default Recipe;
+export default Recipe = withRouter(connect(null, { fetchRecipes })(Recipe));
