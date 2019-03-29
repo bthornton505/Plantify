deee07fd0c22437aabf8dc7685d7e7ba0b5dc721
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-29 10:48:00 -0600

    updated spelling of this file to initialize it

---
+++ pbc-api/app/controllers/api/recipes_controller.rb
@@ -0,0 +1,24 @@
+class Api::RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+    json_response(@recipes)
+  end
+
+  def create
+    @recipe = current_user.recipes.create!(recipe_params)
+    json_response(@recipe)
+  end
+
+  def show
+    @recipe = current_user.recipes.find_by(id: params[:id])
+    json_response(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:title, :description, :ingredients, :cuisine, :user_id)
+  end
+
+end
