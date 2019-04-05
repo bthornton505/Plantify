4366dff59f2ea7fd29ad8afe9ed8c67d8e3d267a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-05 09:09:02 -0600

    added recipereducer to root reducer

--- pbc-client/src/reducers/index.js
+++ pbc-client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducers from './authReducers';
+import recipeReducer from './recipeReducer';
 
 const rootReducer = combineReducers({
-  auth: authReducers
+  auth: authReducers,
+  recipes: recipeReducer
 })
 
 export default rootReducer;
