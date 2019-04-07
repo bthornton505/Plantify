e89c4ff05343b8453cb0c59ed538058d6d8086bf
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-07 15:35:23 -0600

    removed signup and login links

--- pbc-client/src/components/LandingPage.js
+++ pbc-client/src/components/LandingPage.js
@@ -9,9 +9,6 @@ const LandingPage= () => {
       <p>We provide the easiest and way to track your favorite plant-based recipes and share them with friends and family.</p>
       <p>Not only can you create and save your fav recipes, but you can also explore our vast database of vegan/vegetarian recipes!</p>
       <p>If this sounds like something for you, sign up!</p>
-      <Link to="/signup">Sign Up</Link>
-      <br></br>
-      <Link to="/login">Login</Link>
     </div>
   )
 }
