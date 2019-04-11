859ff08a69ac4d721f54f27b4f70d48bb024dad0
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-11 12:15:08 -0600

    created an about page

---
+++ pbc-client/src/components/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const About = () => {
+
+  return(
+    <main>
+      <div>
+        <h1>Why Plantify?</h1>
+
+        <p>
+          Plantify is an application that allows users to not only create and save their
+          most cherised vegan/vegetarian recipes, but a place for them to discover new and exciting recipes.
+        </p>
+
+        <p>
+          I had the inspiration for this app when a friend of mine explained a problem they had where whenver
+          they would go to the grocery story, they couldn't come up with something to buy and make at home (specifically plant-based).
+          They wanted a solution that would allow them to be in the grocery store, search quickly on their phone
+          for a recipe that could incorporate an ingredient they had at home and get a list of ingredients so
+          they could be purchased while they were shopping.
+        </p>
+
+        <p>
+          An app was obviously the solution and I took it upon myself to make it the subject of my final project
+          at the Flatiron School. Hopefully you enjoy this app as much as I do!
+        </p>
+
+      </div>
+    </main>
+  )
+}
+
+export default About;
