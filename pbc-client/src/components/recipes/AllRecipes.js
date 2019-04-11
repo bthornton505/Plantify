ff5976a769a1f1a48ef13d048448fa4baaff8801
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-11 14:20:10 -0600

    finished styling all recipes component and added create link into it

--- pbc-client/src/components/recipes/AllRecipes.js
+++ pbc-client/src/components/recipes/AllRecipes.js
@@ -5,7 +5,7 @@ const AllRecipes = props => {
   // recipeList will map through user recipes and create ordered list of recipes"
   const recipeList = props.recipes.map(recipe => {
     return(
-      <li class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30" key={recipe.id}>
+      <li className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30" key={recipe.id}>
         <Link
           className="black link hover-dark-green"
           to={{
@@ -19,11 +19,14 @@ const AllRecipes = props => {
   })
 
     return(
-      <div class="f4 center mw6 pa3 pa5-ns">
-        <legend class="f4 fw6 ph0 mh0">Recipes: </legend>
-        <ul class="list pl0 measure center">
+      <div className="tc ph4">
+        <h1 className="f3 f2-m f1-l fw2 black-90 mv3">Recipes</h1>
+        <ul className="list pl0 measure center">
           {recipeList}
         </ul>
+        <Link className="black link hover-dark-green" to="/new_recipe">
+          Create Recipe
+        </Link>
       </div>
     )
 }
