0bb7beff567ee6d02bf609285be9551853695d92
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-11 15:23:52 -0600

    styled the landing page but need to tweak the paragraphs

--- pbc-client/src/components/LandingPage.js
+++ pbc-client/src/components/LandingPage.js
@@ -3,12 +3,23 @@ import React from 'react';
 const LandingPage= () => {
 
   return(
-    <div>
-      <h1>Welcome to the Plant Based Chef!</h1>
-      <p>We provide the easiest and way to track your favorite plant-based recipes and share them with friends and family.</p>
-      <p>Not only can you create and save your fav recipes, but you can also explore our vast database of vegan/vegetarian recipes!</p>
-      <p>If this sounds like something for you, sign up!</p>
-    </div>
+    <main>
+      <div className="tc ph4">
+        <h1 className="f3 f2-m f1-l fw2 black-90 mv3">
+          Welcome to the Plantify!
+        </h1>
+
+        <p className="f5 f4-m f3-l fw2 black-80 mt0 lh-copy">
+          We provide the easiest and way to track your favorite plant-based recipes and share them with friends and family.
+        </p>
+        <p className="f5 f4-m f3-l fw2 black-80 mt0 lh-copy">
+          Not only can you create and save your fav recipes, but you can also explore our vast database of vegan/vegetarian recipes!
+        </p>
+        <p className="f5 f4-m f3-l fw2 black-80 mt0 lh-copy">
+          If this sounds like something for you, sign up!
+        </p>
+      </div>
+    </main>
   )
 }
 
