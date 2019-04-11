a7ff95ba67c3009b1076c5aa908e76a6da284140
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-11 16:35:59 -0600

    styled the render of recipes ingredients

--- pbc-client/src/components/recipes/Ingredient.js
+++ pbc-client/src/components/recipes/Ingredient.js
@@ -4,11 +4,13 @@ const Ingredient = props => {
   const ingredients = props.ingredients
   // let ingredientList = ingredients.map((ingredient, index) => <li key={index}>{ingredient}</li>)
 
-  let ingredientList = ingredients ? ingredients.map((ingredient, index) => <li key={index}>{ingredient}</li>) : "Loading"
+  let ingredientList = ingredients ? ingredients.map((ingredient, index) =>
+    <li className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30"
+    key={index}>{ingredient}</li>) : "Loading"
 
   return(
-    <ul>
-      <li>{ingredientList}</li>
+    <ul className="list pl0 measure center">
+      {ingredientList}
     </ul>
   )
 }
