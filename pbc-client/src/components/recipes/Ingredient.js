8958cca7b23259ac825f3508a9da40f7780f1c83
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-12 15:44:35 -0600

    added comment for terniary operator used to render ingredient list

--- pbc-client/src/components/recipes/Ingredient.js
+++ pbc-client/src/components/recipes/Ingredient.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Ingredient = props => {
   const ingredients = props.ingredients
 
+  // this allows the ingredients to wait for the data to arrive from redux and render when it is recieved
   let ingredientList = ingredients ? ingredients.map((ingredient, index) =>
     <li className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30"
     key={index}>{ingredient}</li>) : "Loading"
