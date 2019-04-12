c07f1e1d197088bb9b36f59a4cbccd5b5e05bcfe
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-12 14:50:00 -0600

    added some coments to explain my componentdidmount

--- pbc-client/src/App.js
+++ pbc-client/src/App.js
@@ -18,8 +18,13 @@ import './App.css';
 class App extends Component {
 
   componentWillMount = () => {
+    // This conditional will check the users auth_token after they have logged in and make sure they are authenticated
+
+    // looks in localStorage for a token and if true, it authenticates the token
     if (localStorage.getItem("auth_token")) {
       this.props.checkToken(localStorage.getItem("auth_token"))
+    // if that fails it will check the window location, if location is anywhere but login, signup, or /
+    // it will clear the token and redirect to the login
     } else if (!window.location.href.includes('login') && !window.location.href.includes('signup') && !(window.location.pathname === '/')) {
       localStorage.clear()
       window.location.href = "/login"
@@ -41,7 +46,7 @@ class App extends Component {
           <Route exact path="/login" component={Login} />
         </Switch>
 
-        
+
       </div>
     )
 
