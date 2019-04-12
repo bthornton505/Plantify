6d284ef01386b0f9d3d59e4aa503792f4662d558
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-12 15:43:28 -0600

    removed some unused code

--- pbc-client/src/components/recipes/Ingredient.js
+++ pbc-client/src/components/recipes/Ingredient.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Ingredient = props => {
   const ingredients = props.ingredients
-  // let ingredientList = ingredients.map((ingredient, index) => <li key={index}>{ingredient}</li>)
 
   let ingredientList = ingredients ? ingredients.map((ingredient, index) =>
     <li className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30"
