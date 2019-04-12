af147be389d0cf7516dd0d6fc916f4a7730997ec
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-12 16:41:03 -0600

    removed routes and now rendering Routes component to take care of routes logic

--- pbc-client/src/App.js
+++ pbc-client/src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './containers/Signup';
-import Login from './containers/Login';
-import Navigation from './routes/Navigation'
-// import Footer from './routes/Footer'
-import MyRecipes from './containers/recipes/MyRecipes';
-import RecipeForm from './components/recipes/RecipeForm';
-import Recipe from './components/recipes/Recipe'
-import About from './components/About';
-import LandingPage from './components/LandingPage';
-import UserProfile from './components/UserProfile'
 import { checkToken } from './actions/authActions';
+import Routes from './routes/Routes'
 
 
 class App extends Component {
 
   componentWillMount = () => {
     // This conditional will check the users auth_token after they have logged in and make sure they are authenticated
-
     // looks in localStorage for a token and if true, it authenticates the token
     if (localStorage.getItem("auth_token")) {
       this.props.checkToken(localStorage.getItem("auth_token"))
@@ -31,46 +20,11 @@ class App extends Component {
   }
 
   render() {
-    console.log("App is loading")
     const {isAuthenticated, user} = this.props
-
-    const guestViews = (
-      <div>
-        <Navigation isAuthenticated={isAuthenticated}/>
-
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-
-
-      </div>
-    )
-
-    const userViews = (
-      <div>
-        <Navigation isAuthenticated={isAuthenticated}/>
-
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/user_profile" render={() => <UserProfile user={user} />} />
-          <Route exact path="/my_recipes" render={() => <MyRecipes user={user} />} />
-          <Route exact path="/new_recipe" render={() => <RecipeForm user={user} />} />
-          <Route exact path="/recipe/:id" component={Recipe} />
-        </Switch>
-
-
-      </div>
-    )
     console.log(isAuthenticated)
 
     return (
-      <Router>
-        {isAuthenticated ? userViews : guestViews}
-      </Router>
+      <Routes isAuthenticated={isAuthenticated} user={user}/>
     );
   }
 }
