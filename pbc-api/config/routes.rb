2284b6bef1e6207ac372e6f5a3107e74ac1e1841
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-14 19:24:16 -0600

    created some spacing

--- pbc-api/config/routes.rb
+++ pbc-api/config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, :recipes
+
     post 'signup', to: 'users#create'
+
     post 'auth/login', to: 'authentication#authenticate'
     get 'auth/check_token', to: 'authentication#check_token'
+
     post 'find_user' => 'users#find'
   end
 
