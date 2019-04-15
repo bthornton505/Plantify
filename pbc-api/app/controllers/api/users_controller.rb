df87d34a221c41258e4e7dbd1754c53ab28f7999
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-14 19:23:13 -0600

    removed some commented code

--- pbc-api/app/controllers/api/users_controller.rb
+++ pbc-api/app/controllers/api/users_controller.rb
@@ -7,8 +7,6 @@ class Api::UsersController < ApplicationController
     render json: @users
   end
 
-  # POST /signup
-  # return authenticated token upon signup
   def create
     @user = User.create!(user_params)
     auth_token = AuthenticateUser.new(@user.email, @user.password).call
