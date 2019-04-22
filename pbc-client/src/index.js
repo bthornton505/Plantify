69b0d06a03c37b40bd45a5456dc49d5f3788b3b0
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-22 14:34:05 -0600

    updated redux dev tools code, hopefully help with heroku deployment

--- pbc-client/src/index.js
+++ pbc-client/src/index.js
@@ -8,7 +8,14 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers/index.js'
 // import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
