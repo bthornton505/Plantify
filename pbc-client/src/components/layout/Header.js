695c7831d0dd41ab1b853904a6a7952cbe036b76
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-30 18:14:18 -0600

    created directory to hold layout components

---
+++ pbc-client/src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+
+  return(
+
+      <header className="bg-white black-80 tc pv4 avenir">
+        <h1 className="mt2 mb0 baskerville i fw1 f1">Plantify</h1>
+        <h2 className="mt2 mb0 f6 fw4 ttu tracked">Plantify your life!</h2>
+      </header>
+
+  )
+}
+
+export default Header;
