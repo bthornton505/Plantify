954c296f48206392cc28ab77d158f25aa99c715e
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-30 18:56:46 -0600

    created basic container that will hold the search functionality

---
+++ pbc-client/src/containers/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+  render(){
+    return(
+      <section className="Search">
+        <h1>Search for Recipes!</h1>
+      </section>
+    )
+  }
+}
+
+export default Search;
