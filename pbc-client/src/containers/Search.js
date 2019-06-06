2b3fd5471e03383d3adb2bcc660b179cc33899c9
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-06 13:02:22 -0600

    created search form for a user to submit a query

--- pbc-client/src/containers/Search.js
+++ pbc-client/src/containers/Search.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    search: "",
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
 
   render(){
     return(
       <section className="Search">
         <h1>Search for Recipes!</h1>
+
+        <form id="search-form">
+          <input
+            type="text"
+            name="search"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+        </form>
       </section>
     )
   }
