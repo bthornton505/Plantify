2e09a3f4a0ac9c57e97adfee35a1962dd74c1423
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-06 13:30:23 -0600

    created a component to render the results from the search form

---
+++ pbc-client/src/components/search/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchResults = () => {
+
+  return(
+    <section id="search-results">
+      <p>Results show up here</p>
+    </section>
+  )
+}
+
+export default SearchResults;
