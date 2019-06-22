b4694c5cc5da260ab5d4d894bb32064621731687
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-22 15:07:34 -0600

    building a faraday request which will pull recipes from external api and return them to frontend

--- app/controllers/api/recipe_searches_controller.rb
+++ app/controllers/api/recipe_searches_controller.rb
@@ -1,7 +1,17 @@
 class Api::RecipeSearchesController < ApplicationController
 
-  def search
+  def search_recipes
+    recipes = Faraday.new(:url => "https://api.edamam.com/search")
 
+    recipe_list = recipes.get do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.params['app_id'] = 0b23002f
+      req.params['app_key'] = 9c9063c20f199397dff81682d9cc4e26
+      req.params['']
+    end
+    recipe_hash = JSON.parse(recipe_list.body)
+
+    render json: { recipes: recipe_hash }
   end
 
 end
