b4387dad0c03be3cfe7acb23a6a78366ff781fe2
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-24 17:03:54 -0600

    added a action constant for new recipe search redux action

--- pbc-client/src/actions/actionTypes.js
+++ pbc-client/src/actions/actionTypes.js
@@ -9,3 +9,6 @@ export const GET_RECIPE = "GET_RECIPE"
 export const CREATE_RECIPE = "CREATE_RECIPE"
 export const UPDATE_RECIPE = "UPDATE_RECIPE"
 export const DELETE_RECIPE = "DELETE_RECIPE"
+
+// Edamam API Search Types
+export const SEARCH_RECIPES = "SEARCH_RECIPES"
