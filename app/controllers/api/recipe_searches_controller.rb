fed0d728083114e0ccbd1383b1defde5b0a32308
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-24 17:03:34 -0600

    created a more in depth api call to Edamam api but need to put api keys into secret folder

--- app/controllers/api/recipe_searches_controller.rb
+++ app/controllers/api/recipe_searches_controller.rb
@@ -7,11 +7,17 @@ class Api::RecipeSearchesController < ApplicationController
       req.headers['Content-Type'] = 'application/json'
       req.params['app_id'] = 0b23002f
       req.params['app_key'] = 9c9063c20f199397dff81682d9cc4e26
-      req.params['']
+      req.params['q'] = params[:search]
     end
     recipe_hash = JSON.parse(recipe_list.body)
 
     render json: { recipes: recipe_hash }
+  end
+
+  private
+
+  def search_params
+    params.permit(:health, :search)
   end
 
 end
